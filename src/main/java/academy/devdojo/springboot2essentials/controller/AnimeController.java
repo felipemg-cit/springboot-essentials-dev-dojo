af54f7e8b091070499b8d828b550e5f1c690434d
Felipe Matheus Guimaraes dos Santos <felipemg@ciandt.com>
2022-01-13 14:55:20 -0300

    aula 8

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -6,7 +6,9 @@ import academy.devdojo.springboot2essentials.util.DateUtil;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,13 @@ public class AnimeController {
   private final AnimeService animeService = new AnimeService();
 
   @GetMapping
-  public List<Anime> list() {
+  public ResponseEntity<List<Anime>> list() {
     log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-    return animeService.listAll();
+    return ResponseEntity.ok(animeService.listAll());
+  }
+
+  @GetMapping(path = "/{id}")
+  public ResponseEntity<Anime> findById(@PathVariable long id) {
+    return ResponseEntity.ok(animeService.findById(id));
   }
 }
--- src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -2,12 +2,25 @@ package academy.devdojo.springboot2essentials.service;
 
 import academy.devdojo.springboot2essentials.domain.Anime;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AnimeService {
+  private final List<Anime> animes =
+      List.of(new Anime(0L, "Shingeki no Kyojin"), new Anime(1L, "Kimetsu no Yaiba"));
+
   // private finalAnimeRepository animeRepository;
+
   public List<Anime> listAll() {
-    return List.of(new Anime(0L, "Shingeki no Kyojin"), new Anime(1L, "Kimetsu no Yaiba"));
+    return animes;
+  }
+
+  public Anime findById(long id) {
+    return animes.stream()
+        .filter(anime -> anime.getId().equals(id))
+        .findFirst()
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
   }
 }
