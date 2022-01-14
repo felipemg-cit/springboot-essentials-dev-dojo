1813c941597ef2ac312cf867bd6eb30361f7e4fd
Felipe Matheus Guimaraes dos Santos <felipemg@ciandt.com>
2022-01-14 08:33:59 -0300

    aula 10

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,5 +37,11 @@ public class AnimeController {
   @PostMapping
   public ResponseEntity<Anime> save(@RequestBody Anime anime) {
     return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+  }
+
+  @DeleteMapping(path = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable long id) {
+    animeService.delete(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 }
