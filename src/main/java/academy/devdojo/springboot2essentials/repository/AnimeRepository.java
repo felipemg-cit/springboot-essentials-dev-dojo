869a772c9e94e27350ceb540c04ab209d05612f9
Felipe Matheus Guimaraes dos Santos <felipemg@ciandt.com>
2022-01-19 09:47:31 -0300

    aula 16 - RequestParam

--- src/main/java/academy/devdojo/springboot2essentials/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2essentials/repository/AnimeRepository.java
@@ -1,6 +1,9 @@
 package academy.devdojo.springboot2essentials.repository;
 
 import academy.devdojo.springboot2essentials.domain.Anime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimeRepository extends JpaRepository<Anime, Long> {}
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+  List<Anime> findByName(String name);
+}
